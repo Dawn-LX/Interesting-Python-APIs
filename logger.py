4a351f72b7357841fd8a9dfd2d73696689047676
Kaifeng Gao <kite_phone@zju.edu.cn>
2023-02-28 21:04:44 +0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,17 +1,29 @@
 import time
 import logging
 
+
 _LOG_FMT = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s'
 _DATE_FMT = '%Y-%m-%d %H:%M:%S'
-logging.basicConfig(format=_LOG_FMT, datefmt=_DATE_FMT, level=logging.INFO)
-LOGGER = logging.getLogger('__main__')  # this is the global logger
 
+LOGGER = logging.getLogger('__main__')  # this is the global logger
+LOGGER.setLevel(logging.DEBUG)
 
 def add_log_to_file(log_path):
-    fh = logging.FileHandler(log_path)
     formatter = logging.Formatter(_LOG_FMT, datefmt=_DATE_FMT)
-    fh.setFormatter(formatter)
-    LOGGER.addHandler(fh)
+
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+    LOGGER.addHandler(file_handler)
+
+    terminal_handler = logging.StreamHandler()
+    terminal_handler.setLevel(logging.INFO)
+    terminal_handler.setFormatter(formatter)
+    LOGGER.addHandler(terminal_handler)
+    
+    # so that LOGGER.debug("...") will only be logging to file, not the terminal
+
+    LOGGER.info("-="*20)
 
 
 def get_timestamp():
@@ -26,3 +38,9 @@ def display_args(args):
         txt += "{:{L}}:\t{} {}\n".format(arg_name,arg_value,arg_type,L=max_name_len+1)
     
     return txt
+
+def logger_demo():
+    add_log_to_file("test_APIs/my_log_file2.log")
+    LOGGER.debug("this is debug level, which will only be logged to the file")
+    LOGGER.info("this is info level, which will be logged to both the terminal and the file")
+    
